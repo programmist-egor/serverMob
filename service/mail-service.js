16a17db05a0057cb6837c49480a05b27db5590ae
Egor <egor.starodumov2012@yandex.ru>
2024-12-13 14:00:48 +0700

    Add file server mobile

--- service/mail-service.js
+++ service/mail-service.js
@@ -12,8 +12,8 @@ class MailService {
             port: 465,
             secure: true,
             auth: {
-                user: "info@yooking.ru",
-                pass: "vw4Yn&Zu",
+                user: "info@hottell.ru",
+                pass: "k!DA8s6unlGk",
             },
         });
     }
@@ -21,7 +21,7 @@ class MailService {
     // Отправка письма с кодом подтверждения на указанный email
     async sendConfirmPhoneCodeMail(to, code) {
       return await this.transporter.sendMail({
-            from: "info@yooking.ru",
+            from: "info@hottell.ru",
             to,
             subject: "Код подтверждения ",
             text: "",
@@ -30,7 +30,7 @@ class MailService {
             <div>
                 <h1>Код для подтверждения</h1>
                 <p>Здравствуйте!</p>
-                <p>Поздравляем Вас с регистрацией в мобильном приложении YOOKING!</p>
+                <p>Поздравляем Вас с регистрацией в мобильном приложении HOTTELL!</p>
                 <p>Вы получили это письмо, так как ваш email: ${to} был указан при регистрации.</p>
                 <p>Скопируйте код и подтвердите Вашу почту.</p>
                 <span style="color: #ff6200; font-size: 20px; margin: 20px: font-weight: bold;">${code}</span>
@@ -42,7 +42,7 @@ class MailService {
     // Отправка письма с кодом подтверждения на указанный email для восстановления пароля
     async sendRestorePasswordEmailCodeMail(to, code) {
         return await this.transporter.sendMail({
-            from: "info@yooking.ru",
+            from: "info@hottell.ru",
             to,
             subject: "Восстановление пароля",
             text: "",
@@ -61,7 +61,7 @@ class MailService {
     // Отправка письма с уведомлением, что пароль поменялся
     async sendRestorePasswordMail(to) {
         return await this.transporter.sendMail({
-            from: "info@yooking.ru",
+            from: "info@hottell.ru",
             to,
             subject: "Ваш пароль поменялся",
             text: "",
